71ef277b5133ffd91c540f2373b1933b44e7546e
Ebasa Hulluka Fayisa <ebasahuluka1@gmail.com>
2026-02-04 12:48:16 +0300

    Update App.jsx

--- myCalculator/src/App.jsx
+++ myCalculator/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
 
   function calculate() {
     let result;
+
     const prev = parseFloat(previous);
     const curr = parseFloat(current);
     if (isNaN(prev) || isNaN(curr)) return;
